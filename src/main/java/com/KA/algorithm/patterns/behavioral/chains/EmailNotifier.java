45ad26f75151f51f7320b327264dc292483f7f45
KhodyukevichAndrey <Lolds456@yandex.ru>
2024-03-29 11:26:56 +0400

    add behavioral: chains of responsibility pattern

---
+++ src/main/java/com/KA/algorithm/patterns/behavioral/chains/EmailNotifier.java
@@ -0,0 +1,12 @@
+package com.KA.algorithm.patterns.behavioral.chains;
+
+public class EmailNotifier extends Notifier {
+    public EmailNotifier(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
