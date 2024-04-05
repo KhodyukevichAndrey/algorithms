26e6a9437df847cf24e13be79174fcd8483bd68a
KhodyukevichAndrey <Lolds456@yandex.ru>
2024-04-05 08:49:14 +0400

    add behavioral: memento pattern

---
+++ src/main/java/com/KA/algorithm/patterns/behavioral/memento/GithubRepo.java
@@ -0,0 +1,8 @@
+package com.KA.algorithm.patterns.behavioral.memento;
+
+import lombok.Data;
+
+@Data
+public class GithubRepo {
+    private Save save;
+}
---
+++ src/main/java/com/KA/algorithm/patterns/behavioral/memento/Save.java
@@ -0,0 +1,18 @@
+package com.KA.algorithm.patterns.behavioral.memento;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Save {
+    private final String version;
+    private final Date date;
+
+    public Save(String version) {
+        this.version = version;
+        this.date = new Date();
+    }
+
+
+}
