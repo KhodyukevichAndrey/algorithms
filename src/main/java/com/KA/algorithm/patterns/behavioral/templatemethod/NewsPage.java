49f566454cfea043bfbbba926cb7127c59dd928b
KhodyukevichAndrey <Lolds456@yandex.ru>
2024-04-12 10:16:36 +0400

    add behavioral: template method pattern

--- src/main/java/com/KA/algorithm/patterns/behavioral/templatemethod/NewsPage.java
+++ src/main/java/com/KA/algorithm/patterns/behavioral/templatemethod/NewsPage.java
@@ -1,12 +1,6 @@
 package com.KA.algorithm.patterns.behavioral.templatemethod;
 
 public class NewsPage extends WebsiteTemplate {
-    public void showPage() {
-        System.out.println("Header");
-        showPageContent();
-        System.out.println("Footer");
-    }
-
     @Override
     public void showPageContent() {
         System.out.println("News");
--- src/main/java/com/KA/algorithm/patterns/behavioral/templatemethod/WebsiteTemplate.java
+++ src/main/java/com/KA/algorithm/patterns/behavioral/templatemethod/WebsiteTemplate.java
@@ -3,7 +3,7 @@ package com.KA.algorithm.patterns.behavioral.templatemethod;
 public abstract class WebsiteTemplate {
     public void showPage() {
         System.out.println("Header");
-
+        showPageContent();
         System.out.println("Footer");
     }
 
