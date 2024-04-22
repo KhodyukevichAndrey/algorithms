b6efae62761e548ffe8f24bd219f276e20552246
KhodyukevichAndrey <Lolds456@yandex.ru>
2024-04-22 16:33:49 +0400

    minor updates

--- src/main/java/com/KA/algorithm/AlgorithmApplication.java
+++ src/main/java/com/KA/algorithm/AlgorithmApplication.java
@@ -1,20 +1,80 @@
 package com.KA.algorithm;
 
-import com.KA.algorithm.service.SortService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class AlgorithmApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class, args);
 
-		SortService service = new SortService();
-		service.bubbleSort();
-		service.chosenSort();
-		service.quickSort();
-	}
+        //SortService service = new SortService();
+        //service.bubbleSort();
+        //service.chosenSort();
+        //service.quickSort();
 
 
+        Scanner scanner = new Scanner(System.in);
+        long countOfMarks = scanner.nextInt();
+        long fiveCounter = 0L;
+        long currentRow = 0L;
+        long maxRow = -1L;
+
+        if (countOfMarks < 7){
+            System.out.println(-1);
+        } else {
+            while(scanner.hasNext()){
+                long mark = scanner.nextInt();
+                if(mark == 2 || mark == 3){
+                    fiveCounter = 0L;
+                    currentRow = 0L;
+                }
+                if(mark == 5) {
+                    fiveCounter += 1;
+                    currentRow += 1;
+                }
+                if(currentRow > 6){
+                    maxRow = currentRow;
+                }
+            }
+        }
+        System.out.println(maxRow);
+    }
 }
+/*
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long countOfMarks = scanner.nextInt();
+        long fiveCounter = 0L;
+        long currentRow = 0L;
+        long maxRow = -1L;
+
+        if (countOfMarks < 7){
+            System.out.println(-1);
+        } else {
+            while(scanner.hasNext()){
+                long mark = scanner.nextInt();
+                if(mark == 2 || mark == 3){
+                    fiveCounter = 0L;
+                    currentRow = 0L;
+                }
+                if(mark == 5) {
+                    fiveCounter += 1;
+                    currentRow += 1;
+                }
+                if(currentRow > 6){
+                maxRow = currentRow;
+                }
+            }
+        }
+        System.out.println(maxRow);
+    }
+}
+ */
